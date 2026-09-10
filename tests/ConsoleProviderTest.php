429142a6e229d451f651914a34accfd21dc0b568
dc.To <sdoz@live.com>
2026-09-10 21:54:44 +0800

    add container

--- tests/ConsoleProviderTest.php
+++ tests/ConsoleProviderTest.php
@@ -12,13 +12,14 @@ declare(strict_types=1);
 namespace Puff\Job\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Puff\Di\Container;
 use Puff\Job\ConsoleProvider;
 
 final class ConsoleProviderTest extends TestCase
 {
     public function testRegistersJobGenerator(): void
     {
-        $commands = \iterator_to_array((new ConsoleProvider())->commands(__DIR__));
+        $commands = \iterator_to_array((new ConsoleProvider())->commands(__DIR__, new Container()));
 
         self::assertCount(1, $commands);
         self::assertSame('job', $commands[0]->name());
